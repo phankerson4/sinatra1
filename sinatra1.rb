c171a94d0c64254533db52ffe6d3d7b82ff38262
phankerson4 <phankerson4@gmail.com>
2020-02-12 22:47:40 +0000

    Sinatra Exercise 1

---
+++ sinatra1.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+
+get '/' do 
+  "Hello World!!"
+  end
+ 
+get '/getdoc' do
+  "<!DOCTYPE html>
+  <html>
+  <style>
+  body {
+  color: red;
+    
+  }
+  </style>
+  '<h1>Hello World!!</h1>'
+  <p>this is a sentence</p>
+    </html>"
+end
